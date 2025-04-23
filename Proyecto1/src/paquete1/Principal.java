c8cae7346c2790aa80b97f18ff94f9966d90f554
usuario <usuario@PCPROFE12>
2025-04-23 12:17:12 +0200

    Tercer commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Primer commit");
 		System.out.println("Segundo commit");
+		System.out.println("Tercer commit");
 	}
 
 }
